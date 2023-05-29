a83efec5a31510e3a7435863f67d05061b5ba888
Juan Cruz <juancruzperez933@gmail.com>
2023-05-29 10:14:12 -0300

    Matrix

---
+++ Matrix/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("\n---Matrix---\n");
+int size = 5;
+int[,] matrix2D = new int[size, size];
+for(int i = 0; i < size; i++)
+{
+    Console.Write($"a.{i}: \t");
+    for(int j = 0; j < size; j++)
+    {
+        matrix2D[i, j] = i + j;
+        Console.Write(matrix2D[i, j]+"\t");
+    }
+    Console.WriteLine();
+}
