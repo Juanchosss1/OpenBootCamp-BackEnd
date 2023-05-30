8209c1b663488530d6c55890caff4c966ed03fc7
Juan Cruz <juancruzperez933@gmail.com>
2023-05-30 19:07:50 -0300

    sort

---
+++ Sort and Complexy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+int[] arr;
+Console.WriteLine("Enter number of elements: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+arr = new int[N];
+
+for(int i = 0; i<N; i++)
+{
+    arr[i] = 1;
+}
+Stopwatch crono = new Stopwatch();
+crono.Start();
+
+for(int j = 0; j < N; j++)
+{
+    for(int k = N-1; k > 0; k--)
+    {
+        arr[k] = j+k;
+    }
+}
+crono.Stop();
+Console.WriteLine($"The time elapsed is: {crono.Elapsed}");
