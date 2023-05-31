62f9a23af51e47746c78d53bcd7ce1c42c9a93fe
Juan Cruz <juancruzperez933@gmail.com>
2023-05-31 08:22:16 -0300

    ejercicio3

---
+++ Introduccion a la programacion - Ejercicio-3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = 5;
+        int num2 = 10;
+        int num3 = 3;
+
+        int resultado = SumarNumeros(num1, num2, num3);
+        Console.WriteLine("El resultado de la suma es: " + resultado);
+    }
+
+    static int SumarNumeros(int a, int b, int c)
+    {
+        int suma = a + b + c;
+        return suma;
+    }
+}
+class Coche
+{
+    private int numPuertas;
+
+    public void IncrementarPuertas()
+    {
+        numPuertas++;
+    }
+
+    public int ObtenerNumPuertas()
+    {
+        return numPuertas;
+    }
+    static void Main(string[] args)
+    {
+        Coche miCoche = new Coche();
+        miCoche.IncrementarPuertas();
+        Console.WriteLine("El coche tiene " + miCoche.ObtenerNumPuertas() + " puertas.");
+    }
+
+}
+
+
