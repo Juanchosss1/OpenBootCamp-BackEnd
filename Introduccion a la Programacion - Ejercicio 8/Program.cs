e9bc5508979ccb7c9203aa34486b243100735659
Juan Cruz <juancruzperez933@gmail.com>
2023-05-31 08:40:49 -0300

    ejercicio 8

---
+++ Introduccion a la Programacion - Ejercicio 8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Persona
+{
+    private int edad;
+    private string nombre;
+    private string telefono;
+
+    public int Edad
+    {
+        get { return edad; }
+        set { edad = value; }
+    }
+
+    public string Nombre
+    {
+        get { return nombre; }
+        set { nombre = value; }
+    }
+
+    public string Telefono
+    {
+        get { return telefono; }
+        set { telefono = value; }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Persona persona = new Persona();
+
+        // Utilizando los setters para asignar valores
+        persona.Edad = 25;
+        persona.Nombre = "Juan";
+        persona.Telefono = "123456789";
+
+        // Utilizando los getters para mostrar los valores
+        Console.WriteLine("Edad: " + persona.Edad);
+        Console.WriteLine("Nombre: " + persona.Nombre);
+        Console.WriteLine("Teléfono: " + persona.Telefono);
+    }
+}
